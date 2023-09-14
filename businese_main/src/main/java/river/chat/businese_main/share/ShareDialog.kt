791563f123d97f40f42f4d0376e9dd3fd8cb3e8c
beiyongchao <beiyongchao@dushu365.com>
2023-09-15 02:30:17 +0800

    分享

--- businese_main/src/main/java/river/chat/businese_main/share/ShareDialog.kt
+++ businese_main/src/main/java/river/chat/businese_main/share/ShareDialog.kt
@@ -1,8 +1,15 @@
 package river.chat.businese_main.share
 
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.widget.NestedScrollView
 import com.umeng.socialize.bean.SHARE_MEDIA
 import river.chat.businese_common.report.ShareTracker
 import river.chat.businese_common.report.TrackerEventName
@@ -16,12 +23,15 @@ import river.chat.lib_core.tracker.postTrack
 import river.chat.lib_core.tracker.trackNode
 import river.chat.lib_core.utils.common.QRCodeUtils
 import river.chat.lib_core.utils.exts.ifEmptyOrBlank
-import river.chat.lib_core.utils.exts.view.toBitmap
 import river.chat.lib_core.utils.longan.topActivity
 import river.chat.lib_core.view.main.dialog.BaseBindingDialogFragment
 import river.chat.lib_resource.model.MessageBean
 import river.chat.lib_umeng.ShareManager
 import river.chat.lib_umeng.common.RiverShareContent
+import river.chat.business_main.R
+import river.chat.lib_core.utils.exts.height
+import river.chat.lib_core.utils.exts.view.toBitmap
+import river.chat.lib_core.utils.longan.screenHeight
 
 /**
  * Created by beiyongChao on 2023/6/9
@@ -35,7 +45,7 @@ class ShareDialog(var dialogActivity: AppCompatActivity) :
     private var mBinding: DialogShareBinding? = null
 
     //测试地址
-    private var mTestUrl="https://www.baidu.com/"
+    private var mTestUrl = "https://www.baidu.com/"
 
     companion object {
         @JvmStatic
@@ -44,9 +54,9 @@ class ShareDialog(var dialogActivity: AppCompatActivity) :
 
     override fun onStart() {
         super.onStart()
-        this.trackNode=TrackNode(
+        this.trackNode = TrackNode(
             ShareTracker.KEY_QUESTION to (questionMsg?.content ?: ""),
-            ShareTracker.KEY_ANSWER to ( answerMsg?.content  ?: ""),
+            ShareTracker.KEY_ANSWER to (answerMsg?.content ?: ""),
         )
         postTrack(
             TrackerEventName.LOAD_SHARE,
@@ -58,11 +68,11 @@ class ShareDialog(var dialogActivity: AppCompatActivity) :
         var userPlugin = getPlugin<UserPlugin>()
         var user = userPlugin.getUser()
         mBinding = binding
-        binding.tvQuestion.text = questionMsg?.content ?: ""
-        binding.tvAnswer.text = answerMsg?.content ?: ""
+        binding.includeContent.tvQuestion.text = questionMsg?.content ?: ""
+        binding.includeContent.tvAnswer.text = answerMsg?.content ?: ""
         binding.viewPlatform.mOnPlatformClick = {
-            binding.viewPlatform.trackNode=TrackNode(
-                ShareTracker.KEY_PLATFORM to (it.platform?.name?:"")
+            binding.viewPlatform.trackNode = TrackNode(
+                ShareTracker.KEY_PLATFORM to (it.platform?.name ?: "")
             )
             binding.viewPlatform.postTrack(
                 TrackerEventName.CLICK_SHARE,
@@ -72,10 +82,14 @@ class ShareDialog(var dialogActivity: AppCompatActivity) :
         binding.viewPlatform.mOnCancelClick = {
             closeDialog()
         }
-        binding.ivCode.setImageBitmap(QRCodeUtils.createQRCode(mTestUrl))
-        binding.ivAvatar.loadAvatar(user.headImg )
-        binding.tvName.text=user.nickName.ifEmptyOrBlank("GptEvery用户")
+        binding.includeContent.ivCode.setImageBitmap(QRCodeUtils.createQRCode(mTestUrl))
+        binding.includeContent.ivAvatar.loadAvatar(user.headImg)
+        binding.includeContent.tvName.text = user.nickName.ifEmptyOrBlank("GptEvery用户")
+
+
+        updateView()
     }
+
 
     private fun closeDialog() {
         dismiss()
@@ -99,14 +113,69 @@ class ShareDialog(var dialogActivity: AppCompatActivity) :
     }
 
     private fun share(platformBean: SharePlatformBean) {
+        shareScroll()
         ShareManager.update(
             RiverShareContent().apply {
                 mTitle = "快来提问吧"
                 mText = "快来提问吧"
-                mBitmap = mBinding?.scrollView?.toBitmap()
+                mBitmap = mBinding?.includeContentOutput?.scrollView?.toBitmap()
+//                mBitmap = getScrollScreenShot(mBinding?.includeContent?.scrollView)
+//                mBitmap = shareScroll()
 
             }, platformBean.platform ?: SHARE_MEDIA.WEIXIN
         ).shareImageLocal(topActivity)
     }
 
+
+    fun shareScroll(): Bitmap? {
+        mBinding?.flParent?.visibility = View.GONE
+        var mScrollScreenShot: Bitmap? = null
+
+
+
+//        val scrollView: NestedScrollView = inflate.findViewById(R.id.scrollView)
+//        inflate.post(Runnable {
+//            mScrollScreenShot = getScrollScreenShot(scrollView)
+//        })
+        mScrollScreenShot = mBinding?.includeContentOutput?.llRoot?.toBitmap()
+        return mScrollScreenShot
+    }
+
+    private fun updateView() {
+        var userPlugin = getPlugin<UserPlugin>()
+        var user = userPlugin.getUser()
+        mBinding?.let {
+            it.includeContentOutput.tvQuestion.text = questionMsg?.content ?: ""
+            it.includeContentOutput.tvAnswer.text = answerMsg?.content ?: ""
+            it.includeContentOutput.ivCode
+                .setImageBitmap(QRCodeUtils.createQRCode(mTestUrl))
+            it.includeContentOutput.ivAnswer.loadAvatar(user.headImg)
+            it.includeContentOutput.tvName.text =
+                user.nickName.ifEmptyOrBlank("GptEvery用户")
+            it.includeContentOutput.ivOutputBg.height(screenHeight)
+        }
+
+    }
+
+
+    private fun getScrollScreenShot(view: NestedScrollView?): Bitmap? {
+        var bitmap: Bitmap? = null
+        if (null != view) {
+            var height = 0
+            //正确获取ScrollView
+            for (i in 0 until view.childCount) {
+                height += view.getChildAt(i).height
+            }
+            if (height > 0) {
+                //创建保存缓存的bitmap
+                bitmap = Bitmap.createBitmap(view.width, height, Bitmap.Config.ARGB_8888)
+                //可以简单的把Canvas理解为一个画板 而bitmap就是块画布
+                val canvas = Canvas(bitmap)
+                //把view的内容都画到指定的画板Canvas上
+                view.draw(canvas)
+
+            }
+        }
+        return bitmap
+    }
 }
